a5d6786972a124d912d5a85e2447274cda4b7390
Sydney Ann Mason <smason516@outlook.com>
2019-03-04 12:44:28 -0500

    Basic command line that is partially functional

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 use std::io::{self, Write};
+use std::process::Command;
 
 fn main() {
     loop {
         print!(">>> ");
         io::stdout();
-        let _ = io::stdout().flush();
+        io::stdout().flush().expect("Failed to flush prompt");
 
         let mut line = String::new();
         io::stdin().read_line(&mut line).expect("Failed to read input");
@@ -13,8 +14,18 @@ fn main() {
             break;
         }
         else {
-            println!("{}", line);
+            parse_input(line.trim().to_string());
         }
     }
 }
 
+fn parse_input(line: String) {
+    let split = line.split_whitespace();
+    let words: Vec<&str> = split.collect();
+    execute(words)
+}
+
+fn execute(cmd: Vec<&str>) {
+    let output = Command::new(cmd[0]).args(&cmd[1..]).output().expect("Failed to execute");
+    io::stdout().write_all(&output.stdout).unwrap();
+}
